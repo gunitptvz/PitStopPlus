782b9ae37578902323f793ad60025511b1f23671
gunitptvz <ptvz@inbox.ru>
2017-05-20 01:31:21 +0300

    last commit from 20/05/2017

---
+++ NUnit. PitStop+/DataClass.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using RelevantCodes.ExtentReports;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnit.PitStop_
+{
+    public class DataClass
+    {
+       public IWebDriver browser;
+        public IWebElement element;
+
+        public List<object> TiresSearchParameters(string producer, double width, double protector, double diameter, string season)
+        {
+            List<object> tireschange = new List<object>();
+            tireschange.Add(producer); // производитель
+            tireschange.Add(width); // ширина
+            tireschange.Add(protector); // профиль
+            tireschange.Add(diameter); // диаметр
+            tireschange.Add(season); // сезон
+            return tireschange;
+        }
+    }
+}
--- NUnit. PitStop+/TestClass.cs
+++ NUnit. PitStop+/TestClass.cs
@@ -4,16 +4,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using RelevantCodes.ExtentReports;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace NUnit.PitStop_
 {
     [TestFixture]
-    public class TestClass
+    public class TestClass : DataClass
     {
-        [Test]
+        [Test] // Подбор шин на главной странице
         public void TestMethod()
         {
+            element = browser.FindElement(By.XPath(".//*[@class='pseudo-select w313']"));
+            element.Click();
+            element = browser.FindElement(By.XPath(".//div[@class='pseudo-select w313']//div[@class='options']//div[@value='8793']"));
+            element.Click();
+
+
             
+
+        }
+
+        [SetUp]
+        public void Openbrowser()
+        {
+            browser = new FirefoxDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl("http://pitstopplus.ru/");
+        }
+
+        [TearDown]
+        public void Closebrowser()
+        {
+           // browser.Quit();
         }
     }
 }
