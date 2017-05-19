0582a0db90ee948d6d73273dcd2bd8c6668c425f
gunitptvz <ptvz@inbox.ru>
2017-05-19 15:49:07 +0300

    firs commit

---
+++ NUnit. PitStop+/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.PitStop_
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            
+        }
+    }
+}
