832f10600abc597ea3604ce9cbbeb5dfed8e63b1
gunitptvz <ptvz@inbox.ru>
2017-06-06 21:03:40 +0300

    last commit on 06/06/2017

--- NUnit. PitStop+/DataClass.cs
+++ NUnit. PitStop+/DataClass.cs
@@ -14,28 +14,6 @@ using OpenQA.Selenium.Support.UI;
 
 namespace NUnit.PitStop_
 {
-
-    // Enum Tires brand
-    enum BrandName
-    {
-        Achilles,
-        BFGoodrich,
-        Bridgestone,
-        Continental,
-        Cooper,
-        Dunlop,
-        Gislaved,
-        Goodyear,
-        Hankook,
-        Kumho,
-        MATADOR,
-        Michelin,
-        Nokian,
-        Roadstone,
-        Tigar,
-        Yokohama
-    }
-
     public class DataClass
     {
         // DATA
@@ -45,6 +23,72 @@ namespace NUnit.PitStop_
 
         public static ExtentReports extent;
         public static ExtentTest test;
+
+        // Enum Tires brand
+        public enum BrandName
+        {
+            Achilles,
+            BFGoodrich,
+            Bridgestone,
+            Continental,
+            Cooper,
+            Dunlop,
+            Gislaved,
+            Goodyear,
+            Hankook,
+            Kumho,
+            MATADOR,
+            Michelin,
+            Nokian,
+            Roadstone,
+            Tigar,
+            Yokohama
+        }
+
+        // Enum Tires width
+        public enum TireWidth
+        {
+            seven_point_five,
+            twelve,
+            thirty,
+            thirty_by_nine_point_five,
+            thirtyone,
+            thirtyone_by_ten_point_five,
+            thirtyone_by_ten_point_fifty,
+            thirtytwo,
+            thirtytwo_by_eleven_point_five,
+            thirtytwo_by_eleven_point_fifty,
+            thirtythree,
+            thirtythree_by_twelve_point_five,
+            thirtythree_by_twelve_point_fifty,
+            thirtyfive,
+            thirtyfive_by_twelve_point_five,
+            thirtyseven,
+            one_hundred_thirtyfive,
+            one_hundred_fourtyfive,
+            one_hundred_fiftyfive,
+            one_hundred_sixtyfive,
+            one_hundred_seventyfive,
+            one_hundred_eightyfive,
+            one_hundred_ninty,
+            one_hundred_nintyfive,
+            two_hundred_five,
+            two_hundred_fifteen,
+            two_hundred_twentyfive,
+            two_hundred_thirtyfive,
+            two_hundred_fourty,
+            two_hundred_fourtyfive,
+            two_hundred_fiftyfive,
+            two_hundred_sixtyfive,
+            two_hundred_seventyfive,
+            two_hundred_eightyfive,
+            two_hundred_nintyfive,
+            three_hundred_five,
+            three_hundred_fifteen,
+            three_hundred_twentyfive,
+            three_hundred_thirtyfive,
+            three_hundred_fiftyfive,
+        }
     }
         
     }
--- NUnit. PitStop+/PodborShinMethods.cs
+++ NUnit. PitStop+/PodborShinMethods.cs
@@ -41,7 +41,7 @@ namespace NUnit.PitStop_
         }
 
         // Поиск по бренду
-        public void TiresSearch(Enum brandname)
+        public void TiresSearch(BrandName brandname)
         {
             string xpathselectlist = ".//div[@class='form f1']//div[@class='pseudo-select w313']";
             string xpathselectoption = ".//div[@class='form f1']//div[@class='pseudo-select w313']//div[@class='options']//div";
@@ -77,9 +77,174 @@ namespace NUnit.PitStop_
 
         // Поиск по ширине (+ бренду)
 
-        public void TiresSearch(Enum brandname, string width)
+        public string TiresSearch(BrandName brandname, TireWidth width)
         {
+            switch(width)
+            {
+                case TireWidth.seven_point_five:
+                    {
+                        return "7,5";
+                    }
+                case TireWidth.twelve:
+                    {
+                        return "12";
+                    }
+                case TireWidth.thirty:
+                    {
+                        return "30";
+                    }
+                case TireWidth.thirty_by_nine_point_five:
+                    {
+                        return "30X9.5";
+                    }
+                case TireWidth.thirtyone:
+                    {
+                        return "31";
+                    }
+                case TireWidth.thirtyone_by_ten_point_five:
+                    {
+                        return "31X10.5";
+                    }
+                case TireWidth.thirtyone_by_ten_point_fifty:
+                    {
+                        return "31X10.50";
+                    }
+                case TireWidth.thirtytwo:
+                    {
+                        return "32";
+                    }
+                case TireWidth.thirtytwo_by_eleven_point_five:
+                    {
+                        return "32X11.5";
+                    }
+                case TireWidth.thirtytwo_by_eleven_point_fifty:
+                    {
+                        return "32X11.50";
+                    }
+                case TireWidth.thirtythree:
+                    {
+                        return "33";
+                    }
+                case TireWidth.thirtythree_by_twelve_point_five:
+                    {
+                        return "33X12.5";
+                    }
+                case TireWidth.thirtythree_by_twelve_point_fifty:
+                    {
+                        return "33X12.50";
+                    }
+                case TireWidth.thirtyfive:
+                    {
+                        return "35";
+                    }
+                case TireWidth.thirtyfive_by_twelve_point_five:
+                    {
+                        return "35X12.5";
+                    }
+                case TireWidth.thirtyseven:
+                    {
+                        return "37";
+                    }
+                case TireWidth.one_hundred_thirtyfive:
+                    {
+                        return "135";
+                    }
+                case TireWidth.one_hundred_fourtyfive:
+                    {
+                        return "145";
+                    }
+                case TireWidth.one_hundred_fiftyfive:
+                    {
+                        return "155";
+                    }
+                case TireWidth.one_hundred_sixtyfive:
+                    {
+                        return "165";
+                    }
+                case TireWidth.one_hundred_seventyfive:
+                    {
+                        return "175";
+                    }
+                case TireWidth.one_hundred_eightyfive:
+                    {
+                        return "185";
+                    }
+                case TireWidth.one_hundred_ninty:
+                    {
+                        return "190";
+                    }
+                case TireWidth.one_hundred_nintyfive:
+                    {
+                        return "195";
+                    }
+                case TireWidth.two_hundred_five:
+                    {
+                        return "205";
+                    }
+                case TireWidth.two_hundred_fifteen:
+                    {
+                        return "215";
+                    }
+                case TireWidth.two_hundred_twentyfive:
+                    {
+                        return "225";
+                    }
+                case TireWidth.two_hundred_thirtyfive:
+                    {
+                        return "235";
+                    }
+                case TireWidth.two_hundred_fourty:
+                    {
+                        return "240";
+                    }
+                case TireWidth.two_hundred_fourtyfive:
+                    {
+                        return "245";
+                    }
+                case TireWidth.two_hundred_fiftyfive:
+                    {
+                        return "255";
+                    }
+                case TireWidth.two_hundred_sixtyfive:
+                    {
+                        return "265";
+                    }
+                case TireWidth.two_hundred_seventyfive:
+                    {
+                        return "275";
+                    }
+                case TireWidth.two_hundred_eightyfive:
+                    {
+                        return "285";
+                    }
+                case TireWidth.two_hundred_nintyfive:
+                    {
+                        return "295";
+                    }
+                case TireWidth.three_hundred_five:
+                    {
+                        return "305";
+                    }
+                case TireWidth.three_hundred_fifteen:
+                    {
+                        return "315";
+                    }
+                case TireWidth.three_hundred_twentyfive:
+                    {
+                        return "325";
+                    }
+                case TireWidth.three_hundred_thirtyfive:
+                    {
+                        return "335";
+                    }
+                case TireWidth.three_hundred_fiftyfive:
+                    {
+                        return "355";
+                    }
+            }
 
+            return "0";
+            
         }
 
         // Проверка результатов поиска
@@ -92,5 +257,7 @@ namespace NUnit.PitStop_
             }
             else Assert.Fail("Список шин указанного бренда пустой");
         }
+
+        // Возвращение значений ширины шин
      }
   }
